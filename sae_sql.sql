810a5443528ac8a16d8c459d998b4849cad30e09
Maïken D'Angelo <144994721+MaikenDANGELO@users.noreply.github.com>
2024-01-24 09:06:57 +0100

    Create sae_sql.sql

---
+++ sae_sql.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS gant, taille, type_gants;
+
+
+
+CREATE TABLE taille(
+    id_taille INT,
+    num_taille_fr INT,
+    taille_us INT,
+    tour_de_main INT,
+    PRIMARY KEY (id_taille)
+
+);
+
+CREATE TABLE type_gant (
+    id_type_gant INT,
+    nom_type_gant VARCHAR(255),
+    PRIMARY KEY (id_type_gant)
+
+);
+
+CREATE TABLE gant(
+    id_gant INT AUTO_INCREMENT,
+    nom_gant VARCHAR(255),
+    poids INT,
+    couleur VARCHAR(255),
+    prix_gant VARCHAR(255),
+    taille_id INT,
+    type_gant_id INT,
+    fournisseur VARCHAR(255),
+    marque VARCHAR(255),
+    PRIMARY KEY (id_gant),
+    FOREIGN KEY (taille_id) REFERENCES taille(id_taille),
+    FOREIGN KEY (type_gant_id) REFERENCES type_gant(id_type_gant)
+);
