015e88eb59a5b79d1b1107eaf1f18f753b2408aa
drg0n <146366472+ElenaBERNARD@users.noreply.github.com>
2024-01-25 16:51:16 +0100

    Uwu

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -14,12 +14,14 @@ def client_article_show():                                 # remplace client_ind
     mycursor = get_db().cursor()
     id_client = session['id_user']
 
-    sql = '''   selection des articles   '''
+    sql = '''SELECT * FROM gant;'''
     list_param = []
     condition_and = ""
     # utilisation du filtre
     sql3=''' prise en compte des commentaires et des notes dans le SQL    '''
-    articles =[]
+    
+    mycursor.execute(sql)
+    gants = mycursor.fetchall()
 
 
     # pour le filtre
@@ -34,7 +36,7 @@ def client_article_show():                                 # remplace client_ind
     else:
         prix_total = None
     return render_template('client/boutique/panier_article.html'
-                           , articles=articles
+                           , gants=gants
                            , articles_panier=articles_panier
                            #, prix_total=prix_total
                            , items_filtre=types_article
