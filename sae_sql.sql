082fa26f655b26506b9377edcd08c18251e60db5
drg0n <146366472+ElenaBERNARD@users.noreply.github.com>
2024-02-15 16:19:41 +0100

    update sae.sql

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,78 +1,150 @@
-DROP TABLE IF EXISTS ligne_commande, ligne_panier, commande, etat, gant, taille, type_gant, utilisateur;
+DROP TABLE IF EXISTS liste_envie, commentaire, historique, note, ligne_panier, ligne_commande, commande, adresse, utilisateur, declinaison, gant, type_gant, etat, taille, couleur;
 
-CREATE TABLE utilisateur(
-   id_utilisateur INT AUTO_INCREMENT,
-   login VARCHAR(255),
-   email VARCHAR(255),
-   nom VARCHAR(255),
-   password VARCHAR(255),
-   role VARCHAR(255),
-   PRIMARY KEY(id_utilisateur)
+CREATE TABLE couleur(
+   id_couleur INT NOT NULL AUTO_INCREMENT,
+   libelle_couleur VARCHAR(255),
+   code_couleur VARCHAR(255),
+   PRIMARY KEY(id_couleur)
 );
 
 CREATE TABLE taille(
-    id_taille INT AUTO_INCREMENT,
-    num_taille_fr DECIMAL(3, 1),
-    taille_us VARCHAR(15),
-    tour_de_main DECIMAL(3, 1),
-    PRIMARY KEY (id_taille)
-);
-
-CREATE TABLE type_gant (
-    id_type_gant INT AUTO_INCREMENT,
-    nom_type_gant VARCHAR(255),
-    PRIMARY KEY (id_type_gant)
-);
-
-CREATE TABLE gant(
-    id_gant INT AUTO_INCREMENT,
-    nom_gant VARCHAR(255),
-    poids INT,
-    couleur VARCHAR(255),
-    prix_gant DECIMAL(10, 2),
-    taille_id INT,
-    type_gant_id INT,
-    fournisseur VARCHAR(255),
-    marque VARCHAR(255),
-    image_gant VARCHAR(255),
-    PRIMARY KEY (id_gant),
-    CONSTRAINT fk_gant_taille FOREIGN KEY (taille_id) REFERENCES taille(id_taille),
-    CONSTRAINT fk_gant_type_gant FOREIGN KEY (type_gant_id) REFERENCES type_gant(id_type_gant)
+   id_taille INT NOT NULL AUTO_INCREMENT,
+   num_taille_fr DECIMAL(3, 1),
+   taille_us VARCHAR(255),
+   tour_de_main DECIMAL(4, 1),
+   PRIMARY KEY(id_taille)
 );
 
 CREATE TABLE etat(
-   id_etat INT,
+   id_etat INT NOT NULL AUTO_INCREMENT,
    libelle VARCHAR(255),
    PRIMARY KEY(id_etat)
 );
 
-CREATE TABLE commande(
-   id_commande INT,
-   date_achat DATE,
-   utilisateur_id INT NOT NULL,
-   etat_id INT NOT NULL,
-   PRIMARY KEY(id_commande),
-   CONSTRAINT fk_commande_etat FOREIGN KEY (etat_id) REFERENCES etat(id_etat)
+CREATE TABLE type_gant(
+   id_type_gant INT NOT NULL AUTO_INCREMENT,
+   nom_type_gant VARCHAR(255),
+   PRIMARY KEY(id_type_gant)
 );
 
-CREATE TABLE ligne_panier(
-   gant_id INT,
-   utilisateur_id INT,
-   quantite INT,
-   date_ajout DATE,
-   PRIMARY KEY(gant_id, utilisateur_id),
-   CONSTRAINT fk_ligne_panier_gant FOREIGN KEY (gant_id) REFERENCES gant(id_gant),
-   CONSTRAINT fk_ligne_panier_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id_utilisateur)
+CREATE TABLE gant(
+   id_gant INT NOT NULL AUTO_INCREMENT,
+   nom_gant VARCHAR(255),
+   disponible BIT,
+   poids VARCHAR(255),
+   prix_gant DECIMAL(10, 2),
+   description_gant VARCHAR(255),
+   fournisseur VARCHAR(255),
+   marque VARCHAR(255),
+   image_gant VARCHAR(255),
+   id_type_gant INT NOT NULL,
+   PRIMARY KEY(id_gant),
+   FOREIGN KEY(id_type_gant) REFERENCES type_gant(id_type_gant)
+);
+
+CREATE TABLE declinaison(
+   id_declinaison INT NOT NULL AUTO_INCREMENT,
+   stock INT,
+   prix_declinaison DECIMAL(10, 2),
+   id_couleur INT NOT NULL,
+   id_taille INT NOT NULL,
+   id_gant INT NOT NULL,
+   PRIMARY KEY(id_declinaison),
+   FOREIGN KEY(id_couleur) REFERENCES couleur(id_couleur),
+   FOREIGN KEY(id_taille) REFERENCES taille(id_taille),
+   FOREIGN KEY(id_gant) REFERENCES gant(id_gant)
+);
+
+CREATE TABLE utilisateur(
+   id_utilisateur INT NOT NULL AUTO_INCREMENT,
+   login VARCHAR(255),
+   email VARCHAR(255),
+   password VARCHAR(255),
+   role VARCHAR(255),
+   nom VARCHAR(255),
+   PRIMARY KEY(id_utilisateur)
+);
+
+CREATE TABLE adresse(
+   id_adresse INT NOT NULL AUTO_INCREMENT,
+   nom VARCHAR(255),
+   rue VARCHAR(255),
+   code_postal INT,
+   ville VARCHAR(255),
+   date_utilisation DATE,
+   id_utilisateur INT NOT NULL,
+   PRIMARY KEY(id_adresse),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE commande(
+   id_commande INT NOT NULL AUTO_INCREMENT,
+   date_achat DATE,
+   id_etat INT NOT NULL,
+   id_adresse INT NOT NULL,
+   id_adresse_1 INT NOT NULL,
+   id_utilisateur INT NOT NULL,
+   PRIMARY KEY(id_commande),
+   FOREIGN KEY(id_etat) REFERENCES etat(id_etat),
+   FOREIGN KEY(id_adresse) REFERENCES adresse(id_adresse),
+   FOREIGN KEY(id_adresse_1) REFERENCES adresse(id_adresse),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
 );
 
 CREATE TABLE ligne_commande(
-   gant_id INT,
-   commande_id INT,
-   prix DECIMAL(10,2),
+   id_declinaison INT,
+   id_commande INT,
    quantite INT,
-   PRIMARY KEY(gant_id, commande_id),
-   CONSTRAINT fk_ligne_commande_gant FOREIGN KEY (gant_id) REFERENCES gant(id_gant),
-   CONSTRAINT fk_ligne_commande_commande FOREIGN KEY (commande_id) REFERENCES commande(id_commande)
+   prix INT,
+   PRIMARY KEY(id_declinaison, id_commande),
+   FOREIGN KEY(id_declinaison) REFERENCES declinaison(id_declinaison),
+   FOREIGN KEY(id_commande) REFERENCES commande(id_commande)
+);
+
+CREATE TABLE ligne_panier(
+   id_declinaison INT,
+   id_utilisateur INT,
+   PRIMARY KEY(id_declinaison, id_utilisateur),
+   FOREIGN KEY(id_declinaison) REFERENCES declinaison(id_declinaison),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE note(
+   id_gant INT,
+   id_utilisateur INT,
+   note VARCHAR(255),
+   PRIMARY KEY(id_gant, id_utilisateur),
+   FOREIGN KEY(id_gant) REFERENCES gant(id_gant),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE historique(
+   id_gant INT,
+   id_utilisateur INT,
+   date_consultation VARCHAR(255),
+   PRIMARY KEY(id_gant, id_utilisateur, date_consultation),
+   FOREIGN KEY(id_gant) REFERENCES gant(id_gant),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE commentaire(
+   id_gant INT,
+   id_utilisateur INT,
+   date_publication DATE,
+   commentaire VARCHAR(255),
+   valider BIT,
+   PRIMARY KEY(id_gant, id_utilisateur, date_publication),
+   FOREIGN KEY(id_gant) REFERENCES gant(id_gant),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+
+CREATE TABLE liste_envie(
+   id_gant INT,
+   id_utilisateur INT,
+   date_update DATE,
+   PRIMARY KEY(id_gant, id_utilisateur, date_update),
+   FOREIGN KEY(id_gant) REFERENCES gant(id_gant),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
 );
 
 INSERT INTO taille (num_taille_fr, taille_us, tour_de_main) VALUES
@@ -94,24 +166,58 @@ INSERT INTO type_gant (nom_type_gant) VALUES
     ('Fantaisie'),
     ('Autre');
 
-INSERT INTO gant (nom_gant, poids, couleur, prix_gant, taille_id, type_gant_id, fournisseur, marque, image_gant) VALUES
-    ('Mouffles', 50, 'Noir', 59.99, 6, 3, 'Wedze', 'Wedze', 'mouffle.png'),
-    ('PowerGlove', 500, 'Gris', 169.99, 3, 6, 'CDiscount', 'Nintendo', 'powerglove.png'),
-    ('Gant de toilette', 20, 'Beige', 1.49, 8, 7, 'Linnea', 'Linnea', 'gant_de_toilette.png'),
-    ('Gant de l infini',  10000, 'Or', 999.99, 8, 6, 'Nidavellir', 'Thanos', 'gauntlet_of_infinity.png'),
-    ('Mitaines', 20, 'Noir', 24.99, 4, 2, 'Amazon', 'Satinior', 'mitaine.png'),
-    ('Gants de cuisine', 100, 'Orange', 6.99, 8, 4, 'Temu', 'Cuisinella', 'cuisine.png'),
-    ('Gants de boxe', 285, 'Rose', 23.50, 6, 5, 'Decathlon', 'Decathlon', 'gant_de_boxe.png'),
-    ('Gants de moto', 120, 'Noir', 17.99, 7, 4, 'Amazon', 'Westwood fox', 'moto.png'),
-    ('Gants de gardien de but', 110, 'Rouge', 104.99, 6, 5, 'Amazon', 'T1tan beast', 'foot.png'),
-    ('Gants McDonnalds COLLECTOR 1987', 25, 'Rouge', 599.99, 1, 1, 'Ebay', 'McDonnalds', 'mcdo.png'),
-    ('Gant de baseball', 500, 'Marron', 39.99, 5, 5, 'Amazon', 'Barnett', 'baseball.png'),
-    ('Manchettes-mitaines', 30, 'Rose', 7.61, 2, 2, 'Amazon', 'Bienvenu', 'femboy.png'),
-    ('Gant gratte-chat', 80, 'Bleu', 3.49, 5, 7, 'Amazon', 'Fousenuk', 'chat.png'),
-    ('Gants de chevalier', 1500, 'Argent', 819.99, 7, 6, 'ArmStreet', 'ArmStreet', 'chevalier.png'),
-    ('Gants d astronaute', 460, 'Blanc', 149.99, 6, 7, 'Nasa', 'Nasa', 'astronaute.png');
+INSERT INTO couleur (libelle_couleur, code_couleur) VALUES
+    ('Noir', '#000000'),
+    ('Gris', '#D3D3D3'),
+    ('Beige', '#F5F5DC'),
+    ('Or', '#FFD700'),
+    ('Orange', '#FF8000'),
+    ('Rose', '#FD6C9E'),
+    ('Rouge', '#FF0000'),
+    ('Marron', '#582900'),
+    ('Bleu', '0000FF'),
+    ('Argent', 'C0C0C0'),
+    ('Blanc', 'FFFFFF');
 
-INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom) VALUES
+INSERT INTO gant (nom_gant, disponible, poids, prix_gant, description_gant, fournisseur, marque, image_gant, id_type_gant) VALUES
+    ('Mouffles', 1, 50, 59.99, "Des mouffles d'une qualité exceptionnelle pour garder vos mains au chaud pendant l'hiver." ,'Wedze', 'Wedze', 'mouffle.png', 3),
+    ('PowerGlove', 1, 500, 169.99, "Un gant d'une puissance absolue pour contrôler tous vos jeux à distance ! Plus jamais vous ne voudrez vous séparer du PowerGlove",'CDiscount', 'Nintendo', 'powerglove.png', 6),
+    ('Gant de toilette', 1, 20, 1.49, "Un gant d'une grande douceur pour ne pas abîmer votre peau et garder une douceur de bébé.",'Linnea', 'Linnea', 'gant_de_toilette.png', 7),
+    ('Gant de l infini', 1,  10000, 1999.99, "Prenez en main toute la puissance du gant de l'infini avec cette réplique grandeur nature et réaliste ! Tout est à votre portée !",'Nidavellir', 'Thanos', 'gauntlet_of_infinity.png', 6),
+    ('Mitaines', 1, 20, 24.99, "Ces mitaines vous iront à ravir avec son style exceptionnel.",'Amazon', 'Satinior', 'mitaine.png', 2),
+    ('Gants de cuisine', 1, 100, 6.99, "Ces gants de cuisines épais protègeront vos mains des chaleurs infernales de votre cuisine.",'Temu', 'Cuisinella', 'cuisine.png', 4),
+    ('Gants de boxe', 1, 285, 23.50, "Pour boxer, n'hésiter plus, ces gants sont parfaits pour mettre enchaîner les crochets et les directs avec confort.",'Decathlon', 'Decathlon', 'gant_de_boxe.png', 5),
+    ('Gants de moto', 1, 120, 17.99, "Ces gants de motos sont réalisés avec la maîtrise et l'artisanat d'ouvrier qualifiés basés en Arizona US. N'ayez plus peur d'affronter le goudron !",'Amazon', 'Westwood fox', 'moto.png', 4),
+    ('Gants de gardien de but', 1, 110, 104.99, "Envie de réceptionner des ballons sur le terrain ? Alors ces gants sont parfaits pour vous ! Son cuir élégant vous protègera en tout confort",'Amazon', 'T1tan beast', 'foot.png', 5),
+    ('Gants McDonnalds COLLECTOR 1987', 1, 25, 599.99, "Ces gants proviennent de la collection privée de Ronald McDonald's. Exclusivement produit en 4000 exemplaires en 1987, ce sont des gants collectors, disponibles seulement sur notre site en France.",'Ebay', 'McDonnalds', 'mcdo.png', 1),
+    ('Gant de baseball', 1, 500, 39.99, "Le baseball n'aura plus de secret pour vous avec ces gants. Réceptionner toutes les balles, et ce avec confort et classe.",'Amazon', 'Barnett', 'baseball.png', 5),
+    ('Manchettes-mitaines', 1, 30, 7.61, "Envie de confort et chaleur ? Ces manchettes-mitaines donneront une touche de féminité à votre style ravissant.",'Amazon', 'Bienvenu', 'femboy.png', 2),
+    ('Gant gratte-chat', 1, 80, 3.49, "Votre chat vous fait chier ? Gratter le avec ce gant gratte-chat, et envoyez le au septième ciel !",'Amazon', 'Fousenuk', 'chat.png', 7),
+    ('Gants de chevalier', 1, 1500, 819.99, "Envie de vous battre comme à Azincourt ? N'attendez plus et choisissez les gants de chevalier. Réalisés par un artisan médiéval authentique, ils vous protègeront des flèches et des épées !",'ArmStreet', 'ArmStreet', 'chevalier.png', 6),
+    ('Gants d astronaute', 1, 460, 149.99, "Ces gants collectors sont des répliques fidèles aux gants d'astronautes qui ont posé le pied sur la Lune. N'attendez plus, et envolez vous vers les étoiles !",'Nasa', 'Nasa', 'astronaute.png', 7);
+
+INSERT INTO declinaison (stock, prix_declinaison, id_couleur, id_taille, id_gant) VALUES
+    (10, 59.99, 1, 3, 1),
+    (5, 59.99, 1, 6, 1),
+    (1, 169.99, 2, 6, 2),
+    (18, 1.49, 3, 5, 3),
+    (1, 1999.99, 4, 8, 4),
+    (6, 24.99, 1, 1, 5),
+    (6, 24.99, 1, 4, 5),
+    (2, 6.99, 5, 7, 6),
+    (2, 23.50, 6, 3, 7),
+    (2, 23.99, 9, 6, 7),
+    (6, 17.99, 1, 2, 8),
+    (4, 104.99, 6, 4, 9),
+    (1, 599.99, 7, 1, 10),
+    (12, 39.99, 8, 7, 11),
+    (20, 7.61, 6, 2, 12),
+    (42, 3.49, 9, 5, 13),
+    (2, 819.99, 10, 7, 14),
+    (1, 149.99, 11, 6, 15);
+
+
+INSERT INTO utilisateur(id_utilisateur, login, email, password, role, nom) VALUES
 (1,'admin','admin@admin.fr',
     'sha256$dPL3oH9ug1wjJqva$2b341da75a4257607c841eb0dbbacb76e780f4015f0499bb1a164de2a893fdbf',
     'ROLE_admin','admin'),
@@ -127,33 +233,3 @@ INSERT INTO etat(id_etat, libelle) VALUES
 (2,'expédié'),
 (3,'validé'),
 (4,'confirmé');
-
--- JEU DE TESTS :
-
-SELECT gant.id_gant, gant.nom_gant, gant.couleur, gant.prix_gant, taille.taille_us, type_gant.nom_type_gant
-FROM gant
-JOIN taille ON gant.taille_id = taille.id_taille
-JOIN type_gant ON gant.type_gant_id = type_gant.id_type_gant
-WHERE type_gant.nom_type_gant = 'Mitaine';
-
-
-SELECT gant.id_gant, gant.nom_gant, gant.couleur, gant.prix_gant, taille.taille_us, type_gant.nom_type_gant
-FROM gant
-JOIN taille ON gant.taille_id = taille.id_taille
-JOIN type_gant ON gant.type_gant_id = type_gant.id_type_gant
-ORDER BY taille.num_taille_fr DESC;
-
-
-SELECT type_gant.nom_type_gant, COUNT(gant.id_gant) AS nombre_gants
-FROM gant
-JOIN type_gant ON gant.type_gant_id = type_gant.id_type_gant
-GROUP BY type_gant.nom_type_gant;
-
-
-SELECT taille.taille_us, COUNT(gant.id_gant) AS nombre_gants
-FROM gant
-JOIN taille ON gant.taille_id = taille.id_taille
-GROUP BY taille.taille_us;
-
-SELECT id_utilisateur, nom, email
-FROM utilisateur;
